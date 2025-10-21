56d84577e8ccf52b5315de5cbc9625f722f80edd
richard.deng <richard.deng@live.com>
2025-10-21 18:12:30 +0800

    修改返回数据结构

--- routers/api/v1/trade.go
+++ routers/api/v1/trade.go
@@ -16,15 +16,17 @@ func Trade(c *gin.Context) {
 	requestID := c.Request.Header.Get("X-Request-ID")
 	ctx := context.WithValue(context.Background(), "trace_id", requestID)
 	var requestData format.RequestData
+	ext := make(map[string]interface{})
+	ext["icccondcode"] = ""
+	ext["iccdata"] = ""
 	if err := c.ShouldBindJSON(&requestData); err != nil {
 		logger.Errorf(ctx, "bind json error: %s", err.Error())
 		c.JSON(http.StatusOK, gin.H{
-			"respcd":      e.PARAMERR,
-			"respmsg":     e.GetMsg(e.PARAMERR),
-			"resperr":     e.GetMsg(e.PARAMERR),
-			"chnlsn":      "",
-			"icccondcode": "",
-			"iccdata":     "",
+			"respcd":  e.PARAMERR,
+			"respmsg": e.GetMsg(e.PARAMERR),
+			"resperr": e.GetMsg(e.PARAMERR),
+			"chnlsn":  "",
+			"ext":     ext,
 		})
 		return
 	}
@@ -33,36 +35,34 @@ func Trade(c *gin.Context) {
 	if err != nil {
 		logger.Errorf(ctx, "do transaction error: %s", err.Error())
 		c.JSON(http.StatusOK, gin.H{
-			"respcd":      e.DATAERR,
-			"respmsg":     e.GetMsg(e.DATAERR),
-			"resperr":     e.GetMsg(e.DATAERR),
-			"chnlsn":      "",
-			"icccondcode": "",
-			"iccdata":     "",
+			"respcd":  e.DATAERR,
+			"respmsg": e.GetMsg(e.DATAERR),
+			"resperr": e.GetMsg(e.DATAERR),
+			"chnlsn":  "",
+			"ext":     ext,
 		})
 		return
 	}
-
+	ext["icccondcode"] = ups.Domain38
+	ext["iccdata"] = ups.Domain55
 	if ups.ResponseCode != "00" {
 		code := "10" + ups.ResponseCode
 		msg := e.GetMsg(code)
 		c.JSON(http.StatusOK, gin.H{
-			"respcd":      code,
-			"respmsg":     msg,
-			"resperr":     msg,
-			"chnlsn":      ups.RetrievalReferenceNumber,
-			"icccondcode": ups.Domain38,
-			"iccdata":     ups.Domain55,
+			"respcd":  code,
+			"respmsg": msg,
+			"resperr": msg,
+			"chnlsn":  ups.RetrievalReferenceNumber,
+			"ext":     ext,
 		})
 	} else {
 		msg := e.GetMsg(e.SUCCESS)
 		c.JSON(http.StatusOK, gin.H{
-			"respcd":      e.SUCCESS,
-			"respmsg":     msg,
-			"resperr":     msg,
-			"chnlsn":      ups.RetrievalReferenceNumber,
-			"icccondcode": ups.Domain38,
-			"iccdata":     ups.Domain55,
+			"respcd":  e.SUCCESS,
+			"respmsg": msg,
+			"resperr": msg,
+			"chnlsn":  ups.RetrievalReferenceNumber,
+			"ext":     ext,
 		})
 		return
 	}
